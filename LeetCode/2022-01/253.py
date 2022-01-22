fcd538739e2e1bd45c8756a89421ca930be11ccb
sohee0724 <ayrs02117a@gmail.com>
2022-01-22 22:47:38 +0900

    220121_1문제

---
+++ LeetCode/2022-01/253.py
@@ -0,0 +1,18 @@
+from typing import List
+import heapq
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[0])
+        rooms = []
+        heapq.heappush(rooms, intervals[0][1])
+        for interval in intervals[1:]:
+            if rooms[0]<=interval[0]:
+                heapq.heappop(rooms)
+                
+            heapq.heappush(rooms, interval[1])
+            
+        return len(rooms)
+
+if __name__=='__main__':
+    Solution()
